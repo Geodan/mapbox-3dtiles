a60897990610afc380e75bbe85a69ebcba746691
Tom van Tilburg <tom.van.tilburg@gmail.com>
2020-04-22 20:59:20 +0200

    put code into es6 module in src. use rollup to build iife version

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default {
+        input: ['./src/Mapbox3DTiles.js'],
+        output: {
+                name: 'Mapbox3DTiles',
+                file: './Mapbox3DTiles.js',
+                format: 'iife',
+                sourcemap: true
+        }
+};
