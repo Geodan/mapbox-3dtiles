fb5aa3894b5e176d1e7d370646edf381eae22a8a
Jelle Galgenbeld <jelle.galgenbeld@geodan.nl>
2020-07-24 17:36:22 +0200

    Added stub for rendering
    - Going to draw trianges at the location of the chairs of the arena first and then continue from there

--- Mapbox3DTiles.mjs
+++ Mapbox3DTiles.mjs
@@ -379,8 +379,9 @@ class ThreeDeeTile {
             for (let i=0;i < positions.length /3; i+=3){
               projpos.push(project([positions[i+0],positions[i+1],positions[i+2]]));
             }
-            let normalsRight = new Float32Array(i3dmData.featureTableBinary, i3dmData.featureTableJSON.NORMAL_RIGHT.byteOffset, i3dmData.featureTableJSON.INSTANCES_LENGTH);
-            let normalsUp = new Float32Array(i3dmData.featureTableBinary, i3dmData.featureTableJSON.NORMAL_UP.byteOffset, i3dmData.featureTableJSON.INSTANCES_LENGTH);
+            let normalsRight = new Float32Array(i3dmData.featureTableBinary, i3dmData.featureTableJSON.NORMAL_RIGHT.byteOffset, i3dmData.featureTableJSON.INSTANCES_LENGTH * 3);
+            let normalsUp = new Float32Array(i3dmData.featureTableBinary, i3dmData.featureTableJSON.NORMAL_UP.byteOffset, i3dmData.featureTableJSON.INSTANCES_LENGTH * 3);
+			
             loader.parse(i3dmData.glbData, this.resourcePath, (gltf) => {
               gltf.scene.traverse(child => {
                 if (child instanceof THREE.Mesh) {
@@ -392,7 +393,7 @@ class ThreeDeeTile {
                   child.userData = i3dmData.batchTableJson;
                 }
               });
-              RenderI3dm(gltf, positions, normalsRight , normalsUp);
+			  InstanceRender(gltf, positions, normalsRight , normalsUp);
             });
           } catch (error) {
             console.error(error.message);
@@ -969,11 +970,8 @@ export default class Mapbox3DTiles {
 
 Mapbox3DTiles.Layer = Layer;
 
-function RenderI3dm(gltf, positions, normalsUp, normalsRight) {
-    console.log(gltf);
-    console.log(positions.length);
-    console.log(normalsUp.length);
-    console.log(normalsRight.length);
+function InstanceRender(gltf, positions, normalsUp, normalsRight) {
+	count = position.length / 3;
 
     var meshes = [];
     
@@ -982,5 +980,88 @@ function RenderI3dm(gltf, positions, normalsUp, normalsRight) {
             console.log(child.name);
         }
     });
+	
+	var scene = new THREE.Scene();
+	
+	let matrix = new THREE.Matrix4();
+	matrix.makeRotationX(Math.PI/2);
+	scene.applyMatrix4(matrix);
+	
+	// mesh vertices (replace with the geometry of the actual mesh unindexed if it doesnt work, and converted from geometry to buffergeometry)
+	var vertices = [];
+	
+	// Temporary triangle to replace an actual mesh
+	vertices.push(25, -25, 0);
+	vertices.push(-25, 25, 0);
+	vertices.push(0, 0, 25);
+	
+	var colors = [];
+	for (var i = 0; i < instances; ++i) {
+		colors.push(Math.random(), Math.random(), Math.random(), 1);
+	}
+	
+	var geometry = new THREE.InstancedBufferGeometry();
+	geometry.instanceCount = count;
+		
+	// Load mesh positions into shader.
+	geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices), 3));
+	
+	// Load world positions into shader.
+	geometry.setAttribute('offset', new THREE.InstancedBufferAttribute(positions), 3));
+	// Load colors into shader
+	geometry.setAttribute('color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4));
+	
+	var material = new THREE.RawShaderMaterial({
+		uniforms: {},
+		vertexShader: vertexShader,
+		fragmentShader: fragmentShader,
+		side: THREE.DoubleSide,
+		transparent: false
+	});
+	
+	var mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
 }
 
+var vertexShader =
+	`
+	precision highp float;
+
+	uniform mat4 modelViewMatrix;
+	uniform mat4 projectionMatrix;
+
+	attribute vec3 position;
+	attribute vec3 offset;
+	attribute vec4 color;
+	attribute vec4 rotation;
+
+	varying vec3 vPosition;
+	varying vec4 vColor;
+
+	void main(){
+
+		vPosition = offset + position;
+
+		vColor = color;
+
+		gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
+
+	}
+	`
+;
+
+var fragmentShader =
+	`
+	precision highp float;
+
+	varying vec3 vPosition;
+	varying vec4 vColor;
+
+	void main() {
+
+		vec4 color = vec4( vColor );
+		gl_FragColor = color;
+
+	}
+		`
+;
