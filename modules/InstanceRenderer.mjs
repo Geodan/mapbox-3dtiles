8b71e3b2ebcd92bfd3d0c10b381ea7ca37439b53
Jelle Galgenbeld <jelle.galgenbeld@geodan.nl>
2020-07-29 16:29:35 +0200

    Instanced Rendering code, but inactive. Kept the glPoints in place.
    - The position seems to be correct, but it doesn't draw anything at the moment.
    - Time to do some googling and maybe some interrogating. :-)
    - Potential issues:
      - Shader does not do what it is supposed to do, even though it is one of the simplest ones you can image.
      - Positioning is still wrong.
      - tileContent.add does not allow this type of content, it might be pushed towards the wrong renderer? (highly unlikely)
      - Anything else.

--- modules/InstanceRenderer.mjs
+++ modules/InstanceRenderer.mjs
@@ -25,12 +25,11 @@ function projectToWorld(coords) {
 	}
   
 	var result = new THREE.Vector3(projected[0], projected[1], projected[2]);
-  
 	return result;
-  }
+}
 
   export default function InstanceRender(gltf, positions, normalsRight, normalsUp, inverse) {
-
+		// < Currently necessary projection functions:
 		let project = function(coord){
 			let webmerc = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs';
 			let ecef = '+proj=geocent +datum=WGS84 +units=m +no_defs';
@@ -38,34 +37,127 @@ function projectToWorld(coords) {
 			return newcoord;
 	  }
 
-	  let projpos = []; //WIP: projpos is a temporary hack to have positions reprojected from ECEF to Webmercator
+	  let projpos = [];
 	  for (let i=0;i < positions.length /3; i+=3){
 			projpos.push(project([positions[i+0],positions[i+1],positions[i+2]]));
+		}
+		// >
+
+		// < Gather data from gltf:
+		let gltfMeshes = GetMeshesFromGLTF(gltf);
+		let gltfGeometries = GetGeometriesFromMeshes(gltfMeshes);
+		let gltfMaterials = GetMaterialsFromMeshes(gltfMeshes);
+		// >
+
+		// < Process data into geometries:
+		var meshCount = gltfMeshes.length;
+		for (var i = 0; i < meshCount; ++i) {
+		}
+		// >
+		
+		// < Instance Render Hack (first geometry only)
+		var instanceCount = positions.length / 3;
+		var geometry = gltfGeometries[0];
+
+		var vertices = [];
+		vertices.push(25, -25, 0);
+		vertices.push(-25, 25, 0);
+		vertices.push(0, 0, 25);
+
+		var offsets = [];
+		var color = new THREE.Color();
+		var colors = [];
+
+		// Make first position the origin.
+		var origin = projpos[0];
+
+		for ( var i = 0; i < projpos.length; i ++ ) {
+			// positions (temp hack to substract tileset transform)
+			var x = projpos[i].x - origin.x;
+			var y = projpos[i].y - origin.y;
+			var z = projpos[i].z - origin.z;
+			offsets.push( x, y, z );
+			color.setRGB( Math.random(), Math.random(), Math.random());
+			colors.push( color.r, color.g, color.b );
 	  }
-	  /* Temp hack to show points on positions */
+		
+		var instancedGeometry = new THREE.InstancedBufferGeometry();
+		instancedGeometry.instanceCount = instanceCount;
+		instancedGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+		//instancedGeometry.setAttribute('position', geometry.getAttribute('position'));
+		instancedGeometry.setAttribute('offset', new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3));
+		instancedGeometry.setAttribute('color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4));
+		instancedGeometry.computeVertexNormals();
+		instancedGeometry.computeBoundingBox();
+
+		var material = new THREE.RawShaderMaterial( {
+			uniforms: {},
+			vertexShader: vertexShader,
+			fragmentShader: fragmentShader,
+			side: THREE.DoubleSide,
+			transparent: false
+		});
+
+		// >
+		var mesh = new THREE.Mesh(instancedGeometry, material);
+		// Set the position to the origin to offset the mesh where the geometries are drawn.
+		mesh.position.set(origin.x, origin.y, origin.z);
+		console.log(mesh.position);
+		// ????
+		//return mesh;
+		// ????
+		
 	  var geometry = new THREE.BufferGeometry();
-	  var color = new THREE.Color();
-	  let pos = [];
-	  let colors = [];
+	  let worldpos = [];
+		
 	  for ( var i = 0; i < projpos.length; i ++ ) {
 			// positions (temp hack to substract tileset transform)
-			var x = projpos[i].x;// - 549852;
-			var y = projpos[i].y;// - 6856912;
+			var x = projpos[i].x; // - 549852;
+			var y = projpos[i].y; // - 6856912;
 			var z = projpos[i].z;
-			pos.push( x, y, z );
+			worldpos.push( x, y, z );
 
 			// colors
 			color.setRGB( Math.random(), Math.random(), Math.random());
 			colors.push( color.r, color.g, color.b );
 	  }
-	  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( pos, 3 ) );
+	  geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( worldpos, 3 ) );
 	  geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
 	  geometry.computeBoundingSphere();
 	  geometry.computeBoundingBox();
 	  geometry.applyMatrix4(inverse);
-	  var material = new THREE.PointsMaterial( { size: 15, vertexColors: true } );
-	  return new THREE.Mesh( geometry, material);
-  }
+		var material = new THREE.PointsMaterial( { size: 15, vertexColors: true } );
+		
+	  return new THREE.Mesh(geometry, material);
+}
+
+function GetMeshesFromGLTF( gltf ) {
+	var meshes = [];
+	gltf.scene.traverse(child => {
+		if (child instanceof THREE.Mesh) {
+			meshes.push(child);
+		}
+	});
+	return meshes;
+}
+
+function GetGeometriesFromMeshes( meshes ) {
+	var geometries = [];
+	var meshCount = meshes.length;
+	for (var i = 0; i < meshCount; ++i) {
+		geometries.push(meshes[i].geometry);
+	}
+	return geometries;
+}
+
+function GetMaterialsFromMeshes( meshes ) {
+	var materials = [];
+	var meshCount = meshes.length;
+	for (var i = 0; i < meshCount; ++i) {
+		materials.push(meshes[i].material);
+	}
+	return materials;
+}
 
 // export default function InstanceRender(gltf, positions, normalsUp, normalsRight, inverse) {
 // 	let count = positions.length / 3;
@@ -160,7 +252,6 @@ var vertexShader =
 	attribute vec3 position;
 	attribute vec3 offset;
 	attribute vec4 color;
-	attribute vec4 rotation;
 
 	varying vec3 vPosition;
 	varying vec4 vColor;
@@ -168,7 +259,6 @@ var vertexShader =
 	void main(){
 
 		vPosition = offset + position;
-
 		vColor = color;
 
 		gl_Position = projectionMatrix * modelViewMatrix * vec4( vPosition, 1.0 );
