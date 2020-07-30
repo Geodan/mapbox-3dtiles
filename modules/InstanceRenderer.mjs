a2f0819adc2f284a3a04ea760a981e5594384ed8
Jelle Galgenbeld <jelle.galgenbeld@geodan.nl>
2020-07-30 10:02:44 +0200

    merge

--- modules/InstanceRenderer.mjs
+++ modules/InstanceRenderer.mjs
@@ -29,6 +29,7 @@ export default function InstanceRender(gltf, positions, normalsUp, normalsRight)
 	for (var i = 0; i < instances; ++i) {
 		colors.push(Math.random(), Math.random(), Math.random(), 1);
 	}
+<<<<<<< Updated upstream
 	
 	var geometry = new THREE.InstancedBufferGeometry();
 	geometry.instanceCount = count;
@@ -47,6 +48,127 @@ export default function InstanceRender(gltf, positions, normalsUp, normalsRight)
 		fragmentShader: fragmentShader,
 		side: THREE.DoubleSide,
 		transparent: false
+=======
+  
+	var result = new THREE.Vector3(projected[0], projected[1], projected[2]);
+	return result;
+}
+
+  export default function InstanceRender(gltf, positions, normalsRight, normalsUp, inverse) {
+		// < Currently necessary projection functions:
+		let project = function(coord){
+			let webmerc = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs';
+			let ecef = '+proj=geocent +datum=WGS84 +units=m +no_defs';
+			let newcoord =  proj4(ecef,webmerc,{x:coord[0],y:coord[1],z:coord[2]});
+			return newcoord;
+	  }
+
+	  let projpos = [];
+	  for (let i=0;i < positions.length /3; i+=3){
+			projpos.push(project([positions[i+0],positions[i+1],positions[i+2]]));
+		}
+		// >
+
+		// < Gather data from gltf:
+		let gltfMeshes = GetMeshesFromGLTF(gltf);
+		let gltfGeometries = GetGeometriesFromMeshes(gltfMeshes);
+		let gltfMaterials = GetMaterialsFromMeshes(gltfMeshes);
+		// >
+
+		// < Process data into geometries:
+		var meshCount = gltfMeshes.length;
+		for (var i = 0; i < meshCount; ++i) {
+		}
+		// >
+		
+		// < Instance Render Hack (first geometry only)
+		var instanceCount = positions.length / 3;
+		console.log(instanceCount);
+		var geometry = gltfGeometries[0];
+		var geometry = geometry.toNonIndexed();
+
+		// Instead of a geometry we build a triangle for testing.
+		var vertices = [];
+		vertices.push(0.25, -0.25, 0);
+		vertices.push(-0.25, 0.25, 0);
+		vertices.push(0, 0, 0.25);
+
+		var offsets = [];
+		var color = new THREE.Color();
+		var colors = [];
+
+		// Make first position the origin.
+		var origin = projpos[0];
+
+		for ( var i = 0; i < projpos.length; i ++ ) {
+			// positions (temp hack to substract tileset transform)
+			var x = projpos[i].x - origin.x;
+			var y = projpos[i].y - origin.y;
+			var z = projpos[i].z - origin.z;
+			offsets.push( x, y, z );
+			color.setRGB( Math.random(), Math.random(), Math.random());
+			colors.push( color.r, color.g, color.b );
+	  }
+		
+		var instancedGeometry = new THREE.InstancedBufferGeometry();
+		instancedGeometry.instanceCount = instanceCount;
+		//instancedGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+		instancedGeometry.setAttribute('position', geometry.getAttribute('position'));
+		//console.log(geometry.getAttribute('position'));
+		instancedGeometry.setAttribute('offset', new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3));
+		instancedGeometry.setAttribute('color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4));
+		instancedGeometry.computeVertexNormals();
+		instancedGeometry.computeBoundingBox();
+		instancedGeometry.computeBoundingSphere();
+		instancedGeometry.applyMatrix4(inverse);
+
+		var material = new THREE.RawShaderMaterial( {
+			uniforms: {},
+			vertexShader: vertexShader,
+			fragmentShader: fragmentShader,
+			side: THREE.DoubleSide,
+			transparent: false
+		});
+
+		// >
+		var mesh = new THREE.Mesh(instancedGeometry, material);
+		// Set the position to the origin to offset the mesh where the geometries are drawn.
+		mesh.position.set(origin.x, origin.y, origin.z);
+		return mesh;
+
+	  var geometry = new THREE.BufferGeometry();
+	  let worldpos = [];
+		
+	  for ( var i = 0; i < projpos.length; i ++ ) {
+			// positions (temp hack to substract tileset transform)
+			var x = projpos[i].x - origin.x; // - 549852;
+			var y = projpos[i].y - origin.y; // - 6856912;
+			var z = projpos[i].z - origin.z;
+			worldpos.push( x, y, z );
+
+			// colors
+			color.setRGB( Math.random(), Math.random(), Math.random());
+			colors.push( color.r, color.g, color.b );
+	  }
+		geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( worldpos, 3 ) );
+	  geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
+	  geometry.computeBoundingSphere();
+	  geometry.computeBoundingBox();
+	  geometry.applyMatrix4(inverse);
+		var material = new THREE.PointsMaterial( { size: 15, vertexColors: true } );
+		
+		var weirdmesh = new THREE.Mesh(geometry, material);
+		weirdmesh.position.set(origin.x, origin.y, origin.z);
+	  return weirdmesh;
+}
+
+function GetMeshesFromGLTF( gltf ) {
+	var meshes = [];
+	gltf.scene.traverse(child => {
+		if (child instanceof THREE.Mesh) {
+			meshes.push(child);
+		}
+>>>>>>> Stashed changes
 	});
 	
 	var mesh = new THREE.Mesh(geometry, material);
