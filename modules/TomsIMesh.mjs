a56d1311f355d0c58f17cbe8c3349d41b4b66276
Tom van Tilburg <tom.van.tilburg@gmail.com>
2020-08-26 17:32:39 +0200

    cleaned reprojection stuff

--- modules/TomsIMesh.mjs
+++ modules/TomsIMesh.mjs
@@ -1,20 +1,6 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 
 export async function IMesh(inmesh, positions, normalsRight, normalsUp, inverseMatrix, meshPosition) {
-	/* Temporary projection stuff */
-	let project = function(coord){
-		let webmerc = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs';
-		let ecef = '+proj=geocent +datum=WGS84 +units=m +no_defs';
-		//let newcoord =  proj4(ecef,webmerc,{x:coord[0],y:coord[1],z:coord[2]});
-		let newcoord =  proj4(webmerc,webmerc,{x:coord[0],y:coord[1],z:coord[2]});
-		return newcoord;
-	}
-	let projpos = []; //WIP: projpos is a temporary hack to have positions reprojected from ECEF to Webmercator
-	for (let i=0;i < positions.length / 1; i+=3){
-		let p = project([positions[i+0],positions[i+1],positions[i+2]]);
-		projpos.push(p);
-	}
-	/* END OF Temporary projection stuff */
 	
 	let matrix = new THREE.Matrix4();
 	let position = new THREE.Vector3();
@@ -31,20 +17,20 @@ export async function IMesh(inmesh, positions, normalsRight, normalsUp, inverseM
 	let instancedMesh = new THREE.InstancedMesh( geometry, material, projpos.length );
 	instancedMesh.userData = inmesh.userData;
 
-	for ( var i = 0; i < projpos.length; i++ ) {
+	for ( var i = 0; i < positions.length; i+=3 ) {
 		//TODO: use matix function for this?
 		position = {
-			x: projpos[i].x+ inverseMatrix.elements[12],
-			y: projpos[i].y+ inverseMatrix.elements[13],
-			z: projpos[i].z+ inverseMatrix.elements[14],
+			x: positions[i]+ inverseMatrix.elements[12],
+			y: positions[i+1]+ inverseMatrix.elements[13],
+			z: positions[i+2]+ inverseMatrix.elements[14],
 		}
 		
-		rotation.set(0, 0, Math.atan2(normalsRight[i*3+1],normalsRight[i*3]));
+		rotation.set(0, 0, Math.atan2(normalsRight[i+1],normalsRight[i]));
 		quaternion.setFromEuler( rotation );
-		scale.x = scale.y = scale.z = 1;
+		scale.x = scale.y = scale.z = 1.6;
 
 		matrix.compose( position, quaternion, scale );
-		instancedMesh.setMatrixAt( i, matrix );
+		instancedMesh.setMatrixAt( i/3, matrix );
 	}
 	
 	return instancedMesh;
