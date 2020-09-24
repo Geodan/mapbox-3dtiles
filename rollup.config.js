53f02dfa12ab4dfa2284c534e956e90736f587f2
Anne Blankert <anne.blankert@geodan.nl>
2020-09-24 09:39:23 +0200

    use rollup server for update watch

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import serve from 'rollup-plugin-serve'
 
 export default {
     input: ['./modules/Mapbox3DTiles.mjs'],
@@ -8,5 +9,5 @@ export default {
         format: 'iife',
         sourcemap: true
     },
-    plugins: [nodeResolve()]
+    plugins: [nodeResolve(), serve({port:8082})]
 };
